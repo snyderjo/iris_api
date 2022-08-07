3b868f4ab6a96cad7129a03a6ba10d891e50ff84
John Snyder <snyderjo@gmail.com>
2022-08-07 16:36:41 -0600

    adding the initial stab at the API code.  Testing is still necessary.

---
+++ app.py
@@ -0,0 +1,90 @@
+from flask import Flask, request, jsonify
+from flask_restful import Resource, Api, reqparse
+from joblib import load
+from enum import Enum
+from pandas import read_json
+
+
+app = Flask(__name__)
+api = Api(app)
+
+varOrder = load("X_variable_names.joblib","r")
+class_names = load("class_names.joblib","r")
+
+class ModelHelpers():
+    parser = reqparse.requestParser()
+    #for XVar in varOrder:
+    parser.add_argument("petal length (cm)", type = float, required = True)
+    parser.add_argument("petal width (cm)", type = float, required = True)
+    parser.add_argument("sepal length (cm)", type = float, required = True)
+    parser.add_argument("sepal width (cm)", type = float, required = True)
+
+    def testDataFrame(jsonData):
+        #switch json to a dataframe
+        df = read_json(jsonData)
+        #change the orde of
+        df = df[varOrder]
+        return df
+
+class ModelOutput(Enum):
+    """docstring for ClassName"""
+    PREDICTION = 1
+    PROB_VECTOR = 2
+
+
+class Model_info(Resource):
+    def get(self):
+        return {
+            "models": ["Random_forest","Naive_bayes","SVM","Ensemble"],
+            "necessary features": jsonify(varOrder),
+            "output types": [jsonify(name) for name, value in ModelOutput.__members__.items()]
+        }
+
+
+#all of the below classes could probably benefit from an the use of an Abtract Base Class
+
+class Random_forest(Resource,ModelHelpers):
+    def post(self, output):
+        #grab data
+        data = cls.parser.parse_args()
+        X_test = cls.testDataFrame(data)
+
+        #load the model
+        model = load("rf","rb")
+        if output == PREDICTION:
+            predictions = class_names[model.predict(df)]
+            return jsonify(predictions), 200
+        elif output == PROB_VECTOR:
+            probabilities = pd.dataframe(model.predict_proba(), columns = varOrder)
+            return jsonify(probabilities), 200
+        else:
+            return {"message": "unknown error"}, 404
+
+
+
+class Naive_bayes(Resource,ModelHelpers):
+    def post(self, output):
+        data = cls.parser.parse_args()
+        pass
+
+
+class SVM(Resource,ModelHelpers):
+    def post(self, output):
+        data = cls.parser.parse_args()
+        pass
+
+
+class Ensemble(Resource,ModelHelpers):
+    def post(self, output):
+        data = cls.parser.parse_args()
+        df = cls.
+        pass
+
+
+api.add_resource(Model_list,"/list")
+api.add_resource(Random_forest,"/Random_forest/<ModelOutput:output>")
+api.add_resource(Naive_bayes,"/Naive_bayes/<ModelOutput:output>")
+api.add_resource(SVM,"/SVM/<ModelOutput:output>")
+api.add_resource(Ensemble,"/Ensemble/<ModelOutput:output>")
+
+app.run(port=5000, debug =  True)
